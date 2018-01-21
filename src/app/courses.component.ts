b7792e559ccd070461629321690c2862611b77af
Vitaly Dragun <vdrag00n90@gmail.com>
2018-01-21 17:45:17 +0200

    HelloWorld project - Displaying data and handling events.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
-import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'app-courses',
   template: `
     <h2>{{ title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">{{ course }}</li>
-    </ul>
+    <div (click)="onDivClicked()">
+      <button (click)="onSave($event)">Save</button>
+    </div>
+    <input [value]="email" type="text" (keyup.enter)="email = $event.target.value; onKeyUp()">
+    <input [(ngModel)]="email" type="text" (keyup.enter)="onKeyUp()">
   `
 })
 export class CoursesComponent {
   title = 'List of courses';
-  courses;
+  imageUrl = 'http://lorempixel.com/400/200';
+  colSpan = 2;
+  isActive = false;
+  email = 'me@wxample.com';
 
-  constructor(private service: CoursesService) {
-    this.courses = this.service.getCourses();
+  onSave($event) {
+    $event.stopPropagation();
+    console.log('Button was clicked', $event);
+  }
+
+  onDivClicked() {
+    console.log('Div was clicked');
+  }
+
+  onKeyUp() {
+    console.log(this.email);
   }
 }
