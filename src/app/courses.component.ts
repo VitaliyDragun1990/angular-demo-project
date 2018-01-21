52d02872c5f7c172bc96467ed9766886f783ed3f
Vitaly Dragun <vdrag00n90@gmail.com>
2018-01-21 17:04:30 +0200

    HelloWorld project - Angular fundamentals.

---
+++ src/app/courses.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
+
+@Component({
+  selector: 'app-courses',
+  template: `
+    <h2>{{ title }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">{{ course }}</li>
+    </ul>
+  `
+})
+export class CoursesComponent {
+  title = 'List of courses';
+  courses;
+
+  constructor(private service: CoursesService) {
+    this.courses = this.service.getCourses();
+  }
+}
