e172e7983c28f5fda50d26e2fd9bf4234d9021ff
Vitaly Dragun <vdrag00n90@gmail.com>
2018-01-22 16:08:08 +0200

    Displaying data and handling events - custom pipe.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,19 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-    {{ course.title | uppercase | lowercase }} <br>
-    {{ course.students | number  }} <br>
-    {{ course.rating | number: '1.2-2' }} <br>
-    {{ course.price | currency:'AUD':'symbol':'3.2-2'}} <br>
-    {{ course.releaseDate | date: 'shortDate'}} <br>
+    {{ text | summary: 100 }}
   `
 })
 export class CoursesComponent {
-  course = {
-    title: 'The Complete Angular Course',
-    rating: 4.9745,
-    students: 30123,
-    price: 190.95,
-    releaseDate: new Date(2016, 3, 1)
-  };
+  text = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, laudantium.
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, laudantium.
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, laudantium.
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, laudantium.
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, laudantium.`;
+
 }
