7b8edbaf6c390aa0818b62b98c027dc5303674f2
Vitaly Dragun <vdrag00n90@gmail.com>
2018-01-23 17:04:26 +0200

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [];
-
-  onAdd() {
-    this.courses.push({id: 4, name: 'Course 4'});
-  }
-
-  onChange(course) {
-    course.name = 'UPDATED';
-  }
-
-  loadCourses() {
-    this.courses = [
-      {id: 1, name: 'Course 1'},
-      {id: 2, name: 'Course 2'},
-      {id: 3, name: 'Course 3'},
-    ];
-  }
-
-  trackCourse(index, course) {
-    return course ? course.id : undefined;
-  }
+  canSave = true;
 }
